bf3c062ae565521690a3ea51ea88344c63b0ba1b
Cameron <cameron.wills@ibm.com>
2023-10-11 21:06:40 -0400

    cruise app bulk push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { Router, Route, Switch } from 'wouter';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Preferences from './components/Preferences';
+import Itinerary from './components/Itinerary';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [itineraryPage, setItineraryPage] = useState(false);
+
+  // Determine the component to show based on the user's state
+  const getComponent = () => {
+    if (!user) return <Login />;
+    // You can add more conditions here as your app grows:
+    // e.g., if (!userPreferencesSet) return <Preferences />;
+    return <Itinerary />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          {/* Pass the setUser prop to SignUp */}
+          <Route path='/signup'>
+            <SignUp setUser={setUser} />
+          </Route>
+          <Route path='/login'>
+            <Login setUser={setUser} />
+          </Route>
+          <Route path='/preferences'>
+            <Preferences setItineraryPage={setItineraryPage} />
+          </Route>
+          <Route path='/itinerary' component={Itinerary} />
+          <Route path='/'>{getComponent()}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
